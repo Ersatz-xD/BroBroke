3b6d54f2688682afa11d30d40c64c1f6b00dfbcb
Ayaan Ahmed Khan <aayan.shazim@gmail.com>
2026-03-02 10:56:51 +0500

    new fields to reset pass

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
 module.exports = mongoose.model('User', userSchema);
